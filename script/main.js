3594a6732b080bd97daf7b42bafd58fcbccec16d
DESKTOP-BAFHAUK\YasudaKoudou <tlfei20@fudan.edu.cn>
2022-05-05 21:59:01 +0800

    更新

--- script/main.js
+++ script/main.js
@@ -127,7 +127,8 @@ const animationTimeline = () => {
         opacity: 0,
         y: -150
       },
-      "+=0.7"
+      // "+=0.7"
+      "+=1.2"
     )
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
